4f19da426125e934778c52cd9a682b4dbb73d7a1
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-07-28 02:39:36 +0400

    [feature] - in example users are created

--- example/example.js
+++ example/example.js
@@ -1,8 +1,68 @@
-var mwcCore = require('mwc_kernel');
+var mwcCore = require('mwc_kernel'),
+  async = require('async');
 //setting up the config
 var MWC = new mwcCore(require('./config.json')[(process.env.NODE_ENV) ? (process.env.NODE_ENV) : 'development']);
 
 MWC.usePlugin(require('mwc_plugin_hogan_express'));
 MWC.usePlugin(require('./../index.js'));
 
+MWC.extendRoutes(function (core) {
+  core.app.get('/', function (request, response) {
+    response.json(request.user);
+  });
+});
 MWC.listen();
+
+async.parallel({
+  'vodolaz095': function (cb) {
+    MWC.MODEL.Users.create({
+      'username': 'vodolaz095',
+      'email': 'ostroumov095@gmail.com',
+      'apiKey': 'vodolaz095',
+      'active': true
+    }, cb);
+  },
+  'valmy': function (cb) {
+    MWC.MODEL.Users.create({
+      'username': 'valmy',
+      'email': 'tbudiman@gmail.com',
+      'apiKey': 'valmy',
+      'active': true
+    }, cb);
+  },
+  'muhammadghazali': function (cb) {
+    MWC.MODEL.Users.create({
+      'username': 'muhammadghazali',
+      'email': 'muhammadghazali2480@gmail.com',
+      'apiKey': 'muhammadghazali',
+      'active': true
+    }, cb);
+  },
+  'kaw393939': function (cb) {
+    MWC.MODEL.Users.create({
+      'username': 'kaw393939',
+      'email': 'keith@webizly.com',
+      'apiKey': 'kaw393939',
+      'active': true
+    }, cb);
+  }}, function (err, usersCreated) {
+  if(err) throw err;
+  async.parallel({
+    'pwd4vodolaz095':function(cb){
+      usersCreated.vodolaz095.setPassword('vodolaz095',cb);
+    },
+    'pwd4valmy':function(cb){
+      usersCreated.valmy.setPassword('valmy',cb);
+    },
+    'pwd4muhammadghazali':function(cb){
+      usersCreated.muhammadghazali.setPassword('muhammadghazali',cb);
+    },
+    'pwd4kaw393939':function(cb){
+      usersCreated.kaw393939.setPassword('kaw393939',cb);
+    }
+  },function(err2,passwordSent){
+    if(err2) throw err2;
+    console.log('password were set')
+  })
+});
+
